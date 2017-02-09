4ebf416d77e603af7b65a2490528492530dcb1d6
jimmy <jimmyjob1989@163.com>
2017-02-09 19:59:03 +0800

    修改登录页面样式，添加默认用户

--- app/Modules/Login/Pages/LoginPage/index.js
+++ app/Modules/Login/Pages/LoginPage/index.js
@@ -9,7 +9,8 @@ import { push } from 'react-router-redux';
 import Helmet from 'react-helmet';
 import { createStructuredSelector } from 'reselect';
 
-import { changeUserName, changePassword } from '../../Store/actions';
+import { changeUsername, /*
+changePassword*/ } from 'Modules/Base/Store/actions';
 
 import styles from './styles.css';
 
@@ -24,15 +25,17 @@ export class LoginPage extends React.Component {
 
 	componentDidMount() {
 		document.getElementById('username').focus();
-		const logBtn = document.getElementById('password');
+		const logBtn = document.getElementById('username');
 		logBtn.addEventListener('keyup', this.onKeyup);
+
+		document.getElementById('username').value = 'Jimmy';
 	}
 
 	componentDidUpdate() {
 	}
 
 	componentWillUnmount() {
-		const logBtn = document.getElementById('password');
+		const logBtn = document.getElementById('username');
 		logBtn.removeEventListener('keyup', this.onKeyup, false);
 	}
 
@@ -48,12 +51,17 @@ export class LoginPage extends React.Component {
 		e.preventDefault();
 		const logInfo = {};
 		const username = document.getElementById('username');
+		/*
 		const password = document.getElementById('password');
+		*/
 		logInfo.username = username.value;
+		/*
 		logInfo.password = password.value;
+		*/
 		// 判断一下用户和密码是否补全
-		this.props.changeUserName(logInfo.username);
-		this.props.changePassword(logInfo.password);
+		this.props.changeUsername(logInfo.username);
+		/*
+		this.props.changePassword(logInfo.password);*/
 		if (logInfo.username && logInfo.username !== '') {
 			this.openBasePage();
 		} else {
@@ -76,6 +84,9 @@ export class LoginPage extends React.Component {
 		this.openRoute('/index');
 	};
 
+	/*
+	* <input id="password" type="password" placeholder="密码" />
+	*/
 	render() {
 		return (
 			<div className={styles.login}>
@@ -83,11 +94,8 @@ export class LoginPage extends React.Component {
 				<div className={styles.systemTitle}></div>
 				<div className={styles.loginBox}>
 					<input id="username" type="text" placeholder="用户名" />
-					<br />
-					<input id="password" type="password" placeholder="密码" />
-					<br />
 					{this.state.islogin === 0 ? <div className={styles.error}>请输入用户名</div> : <div className={styles.error}></div>}
-					<button id="loginBtn" onClick={(e) => this.handleSubmit(e)}>登录</button>
+					<button id="loginBtn" onClick={(e) => this.handleSubmit(e)}>下一步</button>
 				</div>
 			</div>
 		);
@@ -96,8 +104,9 @@ export class LoginPage extends React.Component {
 
 LoginPage.propTypes = {
 	changeRoute: React.PropTypes.func,
-	changeUserName: React.PropTypes.func,
-	changePassword: React.PropTypes.func,
+	changeUsername: React.PropTypes.func,
+	/*
+	changePassword: React.PropTypes.func,*/
 };
 
 const mapStateToProps = createStructuredSelector({
@@ -105,8 +114,9 @@ const mapStateToProps = createStructuredSelector({
 
 export function mapDispatchToProps(dispatch) {
 	return {
-		changeUserName: (username) => dispatch(changeUserName(username)),
-		changePassword: (password) => dispatch(changePassword(password)),
+		changeUsername: (username) => dispatch(changeUsername(username)),
+		/*
+		changePassword: (password) => dispatch(changePassword(password)),*/
 		changeRoute: (url) => dispatch(push(url)),
 	};
 }
